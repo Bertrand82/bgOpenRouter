9180b4f092252cb9349c02ea6649b6d596ffa9e9
Bertrand <bertrand.guiral@gmail.com>
2025-07-31 10:58:29 +0200

    save

--- deploy.bat
+++ deploy.bat
@@ -5,63 +5,27 @@ echo.
 echo  ng build github base bgOpenRouter===
 CALL ng build --output-path docsTemp --base-href /bgOpenRouter/
 CALL ng build
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] ng build a échoué.
-    goto fin
-)
-
 echo  ng build 
-
 CALL ng build 
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] ng build a échoué.
-    goto fin
-)
-
-
-
-
-
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] xcopy FILE a échoué.
-    goto fin
-)
 echo  xcopy done 
 echo  xcopy github
 CALL xcopy "" 
 CALL xcopy "docsTemp\browser" "docs" /E /I /H /Y
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] xcopy a échoué.
-    goto fin
-)
-
 
 echo ===  git add . ===
 git add .
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] git add a échoué.
-    goto fin
-)
 
 echo.
 echo  git commit ===
 git commit -m "save"
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] git commit a échoué.
-    goto fin
-)
 
 echo.
 echo  git push ===
 git push
-if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] git push a échoué.
-    goto fin
-)
 
 CALL firebase deploy --only hosting
 if %ERRORLEVEL% neq 0 (
-    echo [ERREUR] firebase a échoué.
+    echo [ERREUR] deploy a échoué.
     goto fin
 )
 echo.
